b15215b59c9a75c81e4ae5b09e9e1001d5baaf8c
RyanD1996 <44706704+RyanD1996@users.noreply.github.com>
2019-11-25 21:16:57 +0000

    Rebase test (#2)
    * Added initial file to feature branch
    
    * Another commit to feature branch
    
    * Another commit to feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('hello this is master')
 
 console.log('editied in my feature branch')
 
+console.log('another addition from feature branch')
 console.log('another addition from master!')
 
 console.log('final addition to master')
